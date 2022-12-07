58afb1216efad28c56b17459f62c0dfcb95c90b1
2231156217@qq.com <2231156217@qq.com>
2022-12-08 00:26:21 +0800

    build: :tada:

---
+++ README.md
@@ -0,0 +1,11 @@
+# 我的博客
+
+采用 nuxt3 框架和 tailwindcss 制作，博客文章通过 notion api 来获取。
+
+## 支持
+
+✅ vue
+✅ ssr
+✅ tailwindcss
+✅ eslint
+✅ prettier
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<script lang="ts" setup></script>
+
+<template>
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+</template>
+
+<style scoped></style>
