9bee201e145d13a738e2913d9beff151ad0ba1d4
2231156217@qq.com <2231156217@qq.com>
2022-12-08 23:56:54 +0800

    feat: :heavy_plus_sign:

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css: ['~/assets/css/main.css'],
+  css: ["~/assets/css/main.css"],
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
+  app: {
+    pageTransition: { name: "page", mode: "out-in" },
+  },
+  modules: ["nuxt-icon"],
 });
