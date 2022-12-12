5a1f8199cabfd444d2666c36f20ac24572ccfd07
2231156217@qq.com <2231156217@qq.com>
2022-12-13 01:13:27 +0800

    feat: 新增社交链接、标签组件、广告1

--- app.vue
+++ app.vue
@@ -1,3 +1,37 @@
+<script setup lang="ts">
+const desc =
+  "欢迎来到我的博客，我会将我在工作、学习中的一些编程经验和技巧已经踩过的坑都记录在这，欢迎指正！";
+const social = [
+  {
+    link: "https://github.com/fightingsun",
+    icon: "simple-icons:github",
+    size: "20px",
+  },
+  {
+    link: "https://gitee.com",
+    icon: "simple-icons:gitee",
+    size: "20px",
+    color: "#c71d23",
+  },
+  {
+    link: "https://www.bilibili.com",
+    icon: "simple-icons:bilibili",
+    size: "22px",
+    color: "#01a3d9",
+  },
+  {
+    link: "https://weibo.com",
+    icon: "simple-icons:sinaweibo",
+    size: "22px",
+  },
+  {
+    link: "https://notion.com",
+    icon: "simple-icons:notion",
+    size: "20px",
+  },
+];
+</script>
+
 <template>
   <header class="flex min-h-fit flex-row items-center justify-between p-16">
     <div>
@@ -41,9 +75,7 @@
           </a>
         </div>
         <!-- 博客介绍 -->
-        <p class="text-lg tracking-widest" style="color: #5f6266">
-          欢迎来到我的博客，我会将我在工作、学习中的一些编程经验和技巧已经踩过的坑都记录在这，欢迎指正！
-        </p>
+        <p class="text-lg tracking-widest" style="color: #5f6266">{{ desc }}</p>
       </section>
       <!-- 搜索框 -->
       <section class="border-b py-12">
@@ -54,10 +86,34 @@
             class="mr-3"
             size="24px"
           />
-          <span class="text-sm tracking-widest" style="color: #535763"
-            >搜索</span
-          >
+          <span class="text-sm tracking-widest" style="color: #535763">
+            搜索
+          </span>
         </div>
+      </section>
+      <!-- 社交链接 -->
+      <section class="border-b py-12">
+        <a
+          v-for="item in social"
+          :key="item.icon"
+          :href="item.link"
+          class="social-link mr-8"
+        >
+          <Icon
+            :name="item.icon"
+            :size="item.size"
+            :style="{ color: item.color ? item.color : '#333' }"
+          />
+        </a>
+      </section>
+      <!-- 广告1 -->
+      <section class="border-b py-12">
+        <a href="/">
+          <img src="~/assets/img/banner1.jpg" alt="banner" class="w-full" />
+        </a>
+      </section>
+      <section class="border-b py-12">
+        <tag-list />
       </section>
     </aside>
     <!-- 内容 -->
@@ -79,5 +135,8 @@
 }
 button:hover {
   opacity: 1;
+}
+.social-link:hover svg {
+  color: #d02f0f !important;
 }
 </style>
---
+++ components/TagList.vue
@@ -0,0 +1,66 @@
+<script lang="ts" setup>
+const tagList = reactive([
+  {
+    label: "前端",
+    link: "http://www.baidu.com",
+  },
+  {
+    label: "Vue",
+    link: "http://www.baidu.com",
+  },
+  {
+    label: "思维导图",
+    link: "http://www.baidu.com",
+  },
+  {
+    label: "读书",
+    link: "http://www.baidu.com",
+  },
+  {
+    label: "NodeJs",
+    link: "http://www.baidu.com",
+  },
+  {
+    label: "视频剪辑",
+    link: "http://www.baidu.com",
+  },
+  {
+    label: "Python",
+    link: "http://www.baidu.com",
+  },
+  {
+    label: "养生",
+    link: "http://www.baidu.com",
+  },
+]);
+</script>
+
+<template>
+  <div class="flex flex-wrap">
+    <NuxtLink
+      v-for="item in tagList"
+      :key="item.label"
+      :href="item.link"
+      class="tag-link"
+    >
+      {{ item.label }}
+    </NuxtLink>
+  </div>
+</template>
+
+<style scoped>
+.tag-link {
+  margin-right: 1rem;
+  margin-bottom: 1rem;
+  padding: 1rem 1.8rem;
+  background-color: #f4f6f8;
+  color: #4b4f55;
+  font-size: 12px;
+  border-radius: 3px;
+  transition: all 200ms ease-out;
+}
+.tag-link:hover {
+  background-color: #d02f0f;
+  color: #fff;
+}
+</style>
